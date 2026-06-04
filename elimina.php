3d47d5d643a2f801fdf863e13c3f55273578df6c
Ruben Isaac Gonzalez Peña <ruben.200831@gmail.com>
2026-06-04 15:28:15 -0600

    feat(del): agrega la funcion eliminar

--- elimina.php
+++ elimina.php
@@ -1,5 +1,21 @@
 <?php
+include 'config.php';
+$clase_mensaje = "";
 
+if ($_SERVER["REQUEST_METHOD"] == 'POST' && isset($_POST['set_a_eliminar'])){
+    $id_set = $_POST["set_a_eliminar"];
+
+    $sql = "DELETE FROM sets WHERE set_num = '$id_set'";   
+    $query = mysqli_query($conexion, $sql);
+    if ($query){
+        $mensaje = "La eliminación fue correcta";
+        $clase_mensaje = "mensaje-exito";
+    }
+    else{
+        $mensaje = "La eliminación no fue correcta";
+        $clase_mensaje = "mensaje-error";
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -17,10 +33,10 @@
     </div>
 
     <!-- PHP --> 
-    <div class="mensaje <?php  ?>">
+    <div class="mensaje <?php echo $clase_mensaje  ?>">
         <h3>Resultado de la operación:</h3>
         <!-- PHP --> 
-        <p><?php  ?></p>
+        <p><?php echo $mensaje  ?></p>
         <br>
         <a href="index.html" style="color: #000; font-weight:bold;">Volver a los resultados de búsqueda</a>
     </div>
